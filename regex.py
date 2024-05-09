1970a884a5de5b565d57df9fb31f087449bd192f
Farzin Jabbary <farzin.jabbary@gmail.com>
2024-05-09 16:29:28 -0700

    Email regex added

---
+++ regex.py
@@ -0,0 +1,12 @@
+import re
+
+text = "Emails: info@gmail.com, support@hotmail.com, customer-service@yahoo.com, customer-support@hotmail.com"
+# Comments for reges:
+    # user_email: Matches one or more word characters (letters, digits, or underscores), dots, or hyphens.
+    # domain: Matches one or more letters (uppercase or lowercase), digits, dots, or hyphens for the domain name part.
+    # (?!hotmail\.com): Exclude all emails that have domain name of hotmail.com
+    # @ and . included in the email address
+    # Top-level domain (TLD): Matches two or more letters (uppercase or lowercase) for the top-level domain
+
+emails = re.findall(r"\b[\w\.-]+@(?!hotmail\.com)[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}\b", text)
+print(emails)
